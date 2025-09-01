0d6402e5496951d208a89fcab9c432982486d6bf
herbertDev12 <herbertn740@gmail.com>
2025-09-01 18:50:46 -0500

    feat: add the cards imgs and descriptions

---
+++ src/GridContainer.tsx
@@ -0,0 +1,99 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+
+export default function GridContainer(){
+
+    return (
+        <div className="max-w-xs">
+          <Card>
+            <CardHeader>
+                <CardTitle>Supervisor </CardTitle>
+                <CardDescription>Monitors activity to identify project roadblocks</CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <img
+                  src="./src/assets/icon-supervisor.svg"
+                />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+                <CardTitle>Team Builder</CardTitle>
+                <CardDescription>Scans our talent network to create the optimal team for your project </CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <img
+                  src="./src/assets/icon-team-builder.svg"
+                />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+                <CardTitle>Karma</CardTitle>
+                <CardDescription>Regularly evaluates our talent to ensure quality</CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <img
+                  src="./src/assets/icon-karma.svg"
+                />
+            </CardFooter>
+          </Card>
+          <Card>
+            <CardHeader>
+                <CardTitle>Calculator </CardTitle>
+                <CardDescription><p>Uses data from past projects to provide better delivery estimates</p>
+                </CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <img
+                  src="./src/assets/icon-calculator.svg"
+                />
+            </CardFooter>
+          </Card>
+        </div>
+    )
+}
+
+/*
+export default function ExtensionGrid(){
+
+    const cards = [
+        { id: 1, src: "./src/assets/images/logo-devlens.svg", name:"DevLens", description:"Quickly inspect page layouts and visualize element boundaries."},
+        { id: 2, src: "src/assets/images/logo-style-spy.svg", name:"StyleSpy", description:"Instantly analize and copy CSS from any webpage element."},
+        { id: 3, src: "src/assets/images/logo-speed-boost.svg", name:"SpeedBoost", description:"Optimizes browser resource usage to accelerate page loading."},
+        { id: 4, src: "src/asset/images/logo-json-wizard.svg", name:"JSONWizard", description:"Formats, validates, and prettifies JSON responses in-browser."},
+        { id: 5, src: "src/assets/images/logo-tab-master-pro.svg", name:"TabMaster Pro", description:"Organizes browser tabs into groups and sessions."},
+        { id: 6, src: "src/assets/images/logo-viewport-buddy.svg", name:"ViewportBuddy", description:"Simulates various screen resolutions directly within the browser."},
+        { id: 7, src: "src/assets/images/logo-markup-notes.svg", name:"Markup Notes", description:"Enables annotation and notes directly onto webpages for collaborative debugging."},
+        { id: 8, src: "src/assets/images/logo-grid-guides.svg", name:"GridGuides", description:"Overlay customizable grids and alignment guides on any webpage"},
+        { id: 9, src: "src/assets/images/logo-palette-picker.svg", name:"Palette Picker", description:"Instanly extracts color palettes from any webpage."},
+        { id: 10, src: "src/assets/images/logo-link-checker.svg", name:"LinkChecker", description:"Scans and highlights broken links on any page."},
+        { id: 11, src: "src/assets/images/ogo-dom-snapshot.svg", name:"DOM Snapshot", description:"Capture and export DOM structures quickly."},
+        { id: 12, src: "src/assets/images/logo-console-plus.svg", name:"ConsolePlus", description:"Enhanced developer console with advanced filtering and logging"}
+    ]
+
+    return (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-4">
+            {cards.map((card) => ( 
+                <ExtensionCard 
+                    key={card.id}
+                    imageSrc={card.src}
+                    cardName={card.name}
+                    cardDescription={card.description}
+                />
+            ))}
+        </div>  
+    )
+}
+
+
+*/
